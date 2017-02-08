50e45d399dce618f681eea6b471dc0a19cb86767
Ryan Collins <ryan@ryanmcollins.com>
2017-02-08 18:40:28 -0500

    Testing TLS Config
    Signed-off-by: Ryan Collins <ryan@ryanmcollins.com>

--- Backend/Sources/main.swift
+++ Backend/Sources/main.swift
@@ -59,6 +59,10 @@ let confData = [
 		[
 			"name":"localhost",
 			"port":port,
+			"tlsConfig": [
+                "certPath":"./localhost.crt",
+                "keyPath":"./localhost.key"
+            ],
 			"routes":[
 				["method":"post", "uri":"/register/device", "handler":handler],
 				["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.staticFiles,
